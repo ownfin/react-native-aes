0ca3d7481621243896f1a4468ca7f5a0f66ee593
Bastian Schöning <bastiapple@googlemail.com>
2021-05-20 19:39:26 +0200

    iOS - Rebranded

--- ios/RNAES/RNAES.m
+++ ios/RNAES/RNAES.m
@@ -1,9 +1,9 @@
 //
-//  RCTAes.m
-//  RCTAes
+//  RNAES.m
 //
 //  Created by tectiv3 on 10/02/17.
 //  Copyright (c) 2017 tectiv3. All rights reserved.
+//  Refactored by bastiandev on 20.05.21.
 //  Copyright (c) 2021 ownfin. All rights reserved.
 //
 
